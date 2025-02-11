ba9b6ce6a7cf931c054c4c27ff1d7fe05cf75231
284618025@qq.com <284618025@qq.com>
2025-02-11 10:57:08 +0800

    第9章 computed属性的两种写法——选项式和函数式的异同

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,46 @@
 <template>
 
-  <div>man2:{{ man2 }}</div>
-  <br>
-  <div>man1:{{ man1 }}</div>
-  <br>
-  <button @click="changeName2">Change Name2</button>
-  <br>
-  <button @click="changeName1">Change Name1</button>
-  <br>
+  <div>
+    <div>
+      姓：<input v-model="firstName" type="text" />
+    </div>
+    <div>
+      名：<input v-model="lastName" type="text" />
+    </div>
+    <div>
+      全名：{{ name }}
+    </div>
+    <!-- <div>
+      <button @click="changeName">修改名字</button>
+    </div> -->
+  </div>
 
 </template>
 
 <script setup lang='ts'>
 
-  import { toRef, reactive, toRefs, toRaw } from 'vue';
+  import { ref, computed } from 'vue';
 
-  const man1 = { name: 'Tom', age: 25, gender: 'Male' }
-  const man2 = reactive({ name: 'Tom', age: 25, gender: 'Male' })
+  let firstName = ref('张');
+  let lastName = ref('三');
 
-  const changeName2 = () => {
-    man2.name = 'Jerry';
-    console.log(toRaw(man2))
-  }
-  const changeName1 = () => {
-    man1.name = 'Jerry';
-    console.log(toRaw(man1))
-  }
-
-  // 手写toRefs源码
-  const myToRefs = <T extends object>(obj: T) => {
-    // 创建一个空对象
-    const map: any = {}
-    // 遍历传入的对象的属性
-    for (const key in obj) {
-      // 判断属性是否是对象自身的属性
-      if (obj.hasOwnProperty(key)) {
-        // 将属性转换为响应式对象，并将key-属性键值对添加到map对象中
-        map[key] = toRef(obj, key)
-      }
-    }
-    return map
-  }
-
-  let { name, age, gender } = myToRefs(man2) // 转换为响应式对象
-  // let { name, age, gender } = myToRefs(man1) // 由于man1是普通对象，所以不能转换为响应式对象，只能使用普通的解构赋值
-  // let { name, age, gender } = toRefs(man2)
-
-  console.log("man-series", name, age, gender)
-  console.log(man1, toRaw(man1))
-  console.log(man2, toRaw(man2))
-  // console.log("man1", name1, age1, gender1)
-  // console.log("man3", name.value, age.value, gender.value)
+  // 1、选项式写法，支持一个对象传入get函数及set函数，来自定义操作
+  // let name = computed<string>({
+  //   get() {
+  //     return `${firstName.value} ${lastName.value}`;
+  //   },
+  //   // .value属性一旦被改变，set函数就会被调用
+  //   // 这里的newVal是修改后的新值
+  //   set(newVal: string) {
+  //     console.log(`newVal: ${newVal}`);
+  //     [firstName.value, lastName.value] = newVal.split('-');
+  //   }
+  // })
+  // 2、函数式写法，方便，但是不支持值修改
+  let name = computed(() => `${firstName.value} ${lastName.value}`);
+  // const changeName = () => {
+  //   name.value = `小小小小-鸟`
+  // }
 
 </script>
 
