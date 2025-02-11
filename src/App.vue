c574396676b3302e3c0a12edc0d0384292631e14
284618025@qq.com <284618025@qq.com>
2025-02-11 15:54:31 +0800

    第9章 computed属性的实际应用场景演示——购物车

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,90 @@
 
   <div>
     <div>
-      姓：<input v-model="firstName" type="text" />
+      <input v-model="keyword" placeholder="搜索" type="text">
     </div>
-    <div>
-      名：<input v-model="lastName" type="text" />
+    <div style="margin-top: 20px;">
+      <table border width="600px" cellpadding="0" cellspacing="0">
+        <thead>
+          <tr>
+            <th>物品名称</th>
+            <th>数量</th>
+            <th>价格</th>
+            <th>总价</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in searchItem" :key="index">
+            <td align="center">{{ item.name }}</td>
+            <td align="center"><button @click="item.count > 1 ? item.count-- : ''">-</button>{{ item.count }}<button
+                @click="item.count++">+</button></td>
+            <td align="center">{{ item.price }}</td>
+            <td align="center">{{ item.count * item.price }}</td>
+            <td align="center"><button @click="del(index)">删除</button></td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="5" align="right">总计：{{ MyFoolTotalPrice }}元</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-    <div>
-      全名：{{ name }}
-    </div>
-    <!-- <div>
-      <button @click="changeName">修改名字</button>
-    </div> -->
   </div>
 
 </template>
 
 <script setup lang='ts'>
+  // computed计算属性的实际应用场景
+  import { reactive, ref, computed } from 'vue';
 
-  import { ref, computed } from 'vue';
+  let $total = ref<number>(0)
 
-  let firstName = ref('张');
-  let lastName = ref('三');
-
-  // 1、选项式写法，支持一个对象传入get函数及set函数，来自定义操作
-  // let name = computed<string>({
-  //   get() {
-  //     return `${firstName.value} ${lastName.value}`;
-  //   },
-  //   // .value属性一旦被改变，set函数就会被调用
-  //   // 这里的newVal是修改后的新值
-  //   set(newVal: string) {
-  //     console.log(`newVal: ${newVal}`);
-  //     [firstName.value, lastName.value] = newVal.split('-');
-  //   }
-  // })
-  // 2、函数式写法，方便，但是不支持值修改
-  let name = computed(() => `${firstName.value} ${lastName.value}`);
-  // const changeName = () => {
-  //   name.value = `小小小小-鸟`
+  let keyword = ref<string>('')
+  const searchItem = computed(() => {
+    return items.filter((item: DataItem) => {
+      return item.name.includes(keyword.value)
+    })
+  })
+  interface DataItem {
+    name: string;
+    count: number;
+    price: number;
+  }
+  const items = reactive<DataItem[]>([
+    { name: '苹果', count: 10, price: 1.5 },
+    { name: '香蕉', count: 20, price: 2.5 },
+    { name: '橘子', count: 30, price: 3.5 },
+    { name: '葡萄', count: 40, price: 4.5 },
+    { name: '西瓜', count: 50, price: 5.5 },
+  ]);
+  // 总价一般用函数来实现
+  // const totalPrice = () => {
+  //   $total.value = items.reduce((prev: number, next: DataItem) => {
+  //     return prev + (next.count * next.price);
+  //   }, 0) // prev是上一个值的意思，故在此初始值为0
   // }
+  // totalPrice()
+  const totalPrice = computed(() => {
+    // reduce用来求合计是比较好的方式
+    return items.reduce((prev: number, next: DataItem) => {
+      return prev + (next.count * next.price);
+    }, 0) // prev是上一个值的意思，故在此初始值为0
+  })
+
+  const MyFoolTotalPrice = computed(() => {
+    // 遍历对象，将对象中的count属性与price属性相乘，并返回总和
+    let sum = 0
+    for (let i = 0; i < items.length; i++) {
+      sum += items[i].count * items[i].price
+    }
+    return sum
+  })
+
+  const del = (index: number) => {
+    items.splice(index, 1)
+  }
 
 </script>
 
