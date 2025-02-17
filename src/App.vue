525e4fd467df0ecf3060e51ee0a08a0bfdcc095b
284618025@qq.com <284618025@qq.com>
2025-02-17 09:09:20 +0800

    watch和watchEffect的API使用，源码就先不写了，效益太低了，先以熟悉为主

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,54 @@
 <template>
 
   <div>
-    <div>
-      <input v-model="keyword" placeholder="搜索" type="text">
-    </div>
-    <div style="margin-top: 20px;">
-      <table border width="600px" cellpadding="0" cellspacing="0">
-        <thead>
-          <tr>
-            <th>物品名称</th>
-            <th>数量</th>
-            <th>价格</th>
-            <th>总价</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in searchItem" :key="index">
-            <td align="center">{{ item.name }}</td>
-            <td align="center"><button @click="item.count > 1 ? item.count-- : ''">-</button>{{ item.count }}<button
-                @click="item.count++">+</button></td>
-            <td align="center">{{ item.price }}</td>
-            <td align="center">{{ item.count * item.price }}</td>
-            <td align="center"><button @click="del(index)">删除</button></td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="5" align="right">总计：{{ MyFoolTotalPrice }}元</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+    case1: <input v-model="mess1" type="text">
+    <br>
+    case2: <input v-model="mess2" type="text">
+    <br>
+    case3: <input v-model="message1.foo.bar.name" type="text">
+    <br>
+    case4: <input v-model="message1.foo.bar.age" type="text">
+    <br>
+    <button @click="stop">停止监听</button>
+    <button @click="pause">暂停监听</button>
+    <button @click="resume">恢复监听</button>
   </div>
 
 </template>
 
 <script setup lang='ts'>
-  // computed计算属性的实际应用场景
-  import { reactive, ref, computed } from 'vue';
+  import { reactive, toRef, ref, watch, watchEffect } from 'vue';
 
-  let $total = ref<number>(0)
+  let mess1 = ref('飞机')
+  let mess2 = ref('杯子')
 
-  let keyword = ref<string>('')
-  const searchItem = computed(() => {
-    return items.filter((item: DataItem) => {
-      return item.name.includes(keyword.value)
-    })
-  })
-  interface DataItem {
-    name: string;
-    count: number;
-    price: number;
-  }
-  const items = reactive<DataItem[]>([
-    { name: '苹果', count: 10, price: 1.5 },
-    { name: '香蕉', count: 20, price: 2.5 },
-    { name: '橘子', count: 30, price: 3.5 },
-    { name: '葡萄', count: 40, price: 4.5 },
-    { name: '西瓜', count: 50, price: 5.5 },
-  ]);
-  // 总价一般用函数来实现
-  // const totalPrice = () => {
-  //   $total.value = items.reduce((prev: number, next: DataItem) => {
-  //     return prev + (next.count * next.price);
-  //   }, 0) // prev是上一个值的意思，故在此初始值为0
-  // }
-  // totalPrice()
-  const totalPrice = computed(() => {
-    // reduce用来求合计是比较好的方式
-    return items.reduce((prev: number, next: DataItem) => {
-      return prev + (next.count * next.price);
-    }, 0) // prev是上一个值的意思，故在此初始值为0
-  })
-
-  const MyFoolTotalPrice = computed(() => {
-    // 遍历对象，将对象中的count属性与price属性相乘，并返回总和
-    let sum = 0
-    for (let i = 0; i < items.length; i++) {
-      sum += items[i].count * items[i].price
+  let message1 = reactive({
+    foo: {
+      bar: {
+        name: "张三",
+        age: 20,
+      }
     }
-    return sum
-  })
+  });
 
-  const del = (index: number) => {
-    items.splice(index, 1)
-  }
+
+  watch(() => message1.foo.bar.name, (newVal, oldVal) => {
+    console.log('message1 changed', newVal, oldVal);
+  })
+  // watchEffect是非惰性的，会在页面加载时就立即执行一次，而不是像watch一样监听到数据变化时才执行·
+  // 它可以返回停止、暂停、恢复监听器的方法，当然watch也可以返回停止、暂停、恢复监听器的方法
+  const { stop, pause, resume } = watchEffect((cleanUp) => {
+    console.log('mess1=========>', mess1.value);
+    console.log('mess2=========>', mess2.value);
+    cleanUp(() => {
+      console.log('before');
+    })
+  },
+    // 这里是配置选项，可以设置flush，默认是pre，即在组件更新之前，先清除上一次的副作用，
+    // 还可以设置挺多东西的，可以去官网看看，比如ontrigger函数
+    {
+      flush: 'post'
+    })
 
 </script>
 
