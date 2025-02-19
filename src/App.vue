e5a509c115643eac9564e5a92b3bb6994e24a0c0
284618025@qq.com <284618025@qq.com>
2025-02-19 09:55:35 +0800

    照着复现经典layout布局，结果效果不对，先搁置了，先看下一章节父子组件的传参

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <template>
-
-  <h1>生命周期</h1>
-  <hr>
-  <!-- 组件绑定v-if是会触发组件的创建-销毁的2个生命周期钩子，而v-show就不会 -->
-  <A v-if="flag"></A>
-  <button @click="flag = !flag">创建-销毁</button>
-  <div class="mysass-test">
-    我滴贵贵
-    <div class="mysass-test__inner">
-      el
-    </div>
-    <div class="mysass-test--success">
-      hihi
-    </div>
-  </div>
-
+  <Layout></Layout>
 </template>
 
 <script setup lang='ts'>
-  import { ref } from 'vue';
-  import A from './components/A.vue'
-  const flag = ref(false)
+  import Layout from './Layout/index.vue'
 
 </script>
 
 <!-- scoped表示只作用于当前组件，不会影响全局，而App.vue一般不加scoped，因为里面的东西都影响全局 -->
-<style lang="scss" scoped>
-  @include b(test) {
-    color: red;
-
-    @include e(inner) {
-      color: blue;
-    }
-
-    @include m(success) {
-      color: green;
-    }
-  }
+<style lang="scss">
+  // #app {
+  //   @include bfc;
+  // }
 </style>
---
+++ src/Layout/Menu/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="mysass-menu">Menu</div>
+
+</template>
+
+<script setup lang='ts'>
+
+    import { ref } from 'vue';
+
+    const count = ref(0);
+
+</script>
+
+<style scoped>
+    @include b(menu) {
+        min-width: 300px;
+        border-right: 1px solid #eee;
+        height: 100%;
+    }
+</style>
