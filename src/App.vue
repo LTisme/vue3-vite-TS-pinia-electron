6d16f5f49d6689c1b4ea64fca45e1d340b3f0eb9
284618025@qq.com <284618025@qq.com>
2025-02-24 16:47:51 +0800

    了解了常用的插槽写法

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <template>
-  <div style="display: flex;">
-    <!-- class属性可以有2个，但必须是一个静态一个动态，不能两个静态或者两个动态，否则会报错 -->
-    <div @click="switchComponent(item, index)" :class="[activeIndex == index ? 'active' : '']" class="tab"
-      v-for="(item, index) in data">
-      <div>{{ item.name }}</div>
-    </div>
+  <div class="content">
+    <DialogVue>
+      <!-- 还有动态插槽的写法，当然也可以用简写的形式 -->
+      <template v-slot:[dynamicalSlot]>
+        <div>我被父组件插入了下间</div>
+      </template>
+      <!-- 简写的话是#default="{ customedVarFromChildSlot }" -->
+      <template v-slot="{ customedVarFromChildSlot }"><!-- 解构出来 -->
+        <div>我被父组件插入了中间</div>
+        <div>{{ customedVarFromChildSlot }}</div>
+      </template>
+      <!-- v-slot是可以简写的，这里相当于v-slot:header -->
+      <template #header>
+        <div>我被父组件插入了上面</div>
+      </template>
+    </DialogVue>
   </div>
-  <component :is="dynamicComponent"></component>
 </template>
 
 <script setup lang='ts'>
-  import { ref, reactive, shallowRef, markRaw } from 'vue'
-  import AVue from './components/components-study/A.vue'
-  import BVue from './components/components-study/B.vue'
-  import CVue from './components/components-study/C.vue'
+  import { ref } from 'vue'
+  import DialogVue from './components/Dialog-slot-study/index.vue'
 
-  const activeIndex = ref(0)
+  const dynamicalSlot = ref('footer')
 
-  const switchComponent = (item: any, index: any) => {
-    dynamicComponent.value = item.com
-    activeIndex.value = index
-  }
-
-  // 因为ref的话会对组件很多不必要的信息做一个劫持，所以要跳过它
-  // 所以这里使用shallowRef和markRaw就是为了减少性能开销
-  const dynamicComponent = shallowRef(AVue)
-  const data = reactive([
-    {
-      name: 'A组件',
-      com: markRaw(AVue)
-    },
-    {
-      name: 'B组件',
-      com: markRaw(BVue)
-    },
-    {
-      name: 'C组件',
-      com: markRaw(CVue)
-    }
-  ])
 </script>
 
 <style lang='scss'>
@@ -56,16 +41,5 @@
   * {
     padding: 0;
     margin: 0;
-  }
-
-  .active {
-    background: skyblue;
-  }
-
-  .tab {
-    border: 1px solid #ccc;
-    padding: 5px 10px;
-    margin: 5px;
-    cursor: pointer;
   }
 </style>
---
+++ src/components/Dialog-slot-study/index.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <div>
+        <header class="header">
+            <!-- 演示具名插槽 -->
+            <slot name="header"></slot>
+        </header>
+        <main class="main">
+            <!-- 演示如何通过slot给父组件传值 -->
+            <div v-for="item in dialogSlot">
+                <!-- 演示匿名插槽，也就是默认的插槽 -->
+                <slot :customedVarFromChildSlot="item"></slot>
+            </div>
+        </main>
+        <footer class="footer">
+            <!-- 演示具名插槽 -->
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+
+</template>
+
+<script setup lang='ts'>
+
+    import { ref, reactive } from 'vue';
+
+    const count = ref(0);
+
+    // 演示插槽的作用域
+
+    type DialogSlotType = {
+        name: string;
+        age: number;
+    }
+    const dialogSlot = reactive<DialogSlotType[]>([
+        { name: '小满被插入了', age: 20 },
+        { name: '小明被插入了', age: 25 },
+        { name: '小红被插入了', age: 30 },
+        { name: '小刚被插入了', age: 35 },
+    ])
+
+</script>
+
+<style scoped lang='scss'>
+    .header {
+        height: 200px;
+        background: red;
+        color: white;
+    }
+
+    .main {
+        height: 500px;
+        background: green;
+        color: white;
+    }
+
+    .footer {
+        height: 100px;
+        background: blue;
+        color: white;
+    }
+</style>
