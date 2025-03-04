11f236327f2e2d51ea8079dd633c7d24043ab651
284618025@qq.com <284618025@qq.com>
2025-03-04 15:10:49 +0800

    了解了transition组件结合第三方动画样式animate.css的用法

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="content">
         <button @click="switchFlag = !switchFlag" type="button">切换</button>
-        <Transition name="fade">
+        <Transition enter-active-class="animate__animated animate__rubberBand"
+            leave-active-class="animate__animated animate__jello">
             <div v-if="switchFlag" class="box"></div>
         </Transition>
     </div>
 </template>
 
 <script setup lang="ts">
+    import "animate.css"
+
     import { ref } from 'vue'
     const switchFlag = ref(true)
 </script>
@@ -20,7 +23,7 @@
     }
 
     /* 定义过渡动画效果:from-进入动画效果,active-活动状态, to-离开动画效果 */
-    .fade-enter-from {
+    /* .fade-enter-from {
         width: 0px;
         height: 0px;
         transform: rotate3d(1, 1, 0, 180deg);
@@ -48,5 +51,8 @@
     .fade-leave-to {
         width: 0px;
         height: 0px;
-    }
+    } */
+    /* 除了上面的写法还有直接在标签里写属性，然后可以自定义名的方式，这种方式可以用来结合第三方库一起使用 */
+    /* 这里结合animate.css来实现好看的动画效果 */
+
 </style>
