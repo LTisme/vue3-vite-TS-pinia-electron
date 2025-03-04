8a334d5a52d2d93120aff2f65244985132b7d447
284618025@qq.com <284618025@qq.com>
2025-03-04 14:22:02 +0800

    了解了transition组件的基础使用用法

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
-    <el-button type="primary" @click="switchFlag = !switchFlag">切换组件</el-button>
-    <!-- include是想缓存组件的名称，是vue文件的文件名哈，不是import进来的组件名称 -->
-    <!-- exclude是不想缓存组件的名称 -->
-    <!-- 其实还有max属性，表示缓存的最大数量，采用了lru算法 -->
-    <!-- 然后使用了keep-alive的话，会添加2个生命周期onActivated和onDeactivated -->
-    <!-- keep-alive组件的子组件必须是单文件组件，下面两个如果不用if-else的话会报错 -->
-    <!-- <Avue /> -->
-    <!-- <Bvue /> -->
-    <keep-alive :include="['B', 'A']">
-        <Avue v-if="switchFlag" />
-        <Bvue v-else />
-    </keep-alive>
+    <div class="content">
+        <button @click="switchFlag = !switchFlag" type="button">切换</button>
+        <Transition name="fade">
+            <div v-if="switchFlag" class="box"></div>
+        </Transition>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue'
-    import Avue from '@/components/study_keep-alive组件/A.vue'
-    import Bvue from '@/components/study_keep-alive组件/B.vue'
     const switchFlag = ref(true)
 </script>
 
-<style scoped></style>
+<style scoped>
+    .box {
+        width: 200px;
+        height: 200px;
+        background-color: red;
+    }
+
+    /* 定义过渡动画效果:from-进入动画效果,active-活动状态, to-离开动画效果 */
+    .fade-enter-from {
+        width: 0px;
+        height: 0px;
+        transform: rotate3d(1, 1, 0, 180deg);
+    }
+
+    .fade-enter-active {
+        transition: all 1.5s ease;
+    }
+
+    .fade-enter-to {
+        width: 200px;
+        height: 200px;
+    }
+
+    .fade-leave-from {
+        width: 200px;
+        height: 200px;
+        transform: rotate(360deg);
+    }
+
+    .fade-leave-active {
+        transition: all 5s ease;
+    }
+
+    .fade-leave-to {
+        width: 0px;
+        height: 0px;
+    }
+</style>
