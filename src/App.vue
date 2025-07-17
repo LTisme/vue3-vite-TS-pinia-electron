700352379e0c7d0fcd3e208246fd6ec8ad522965
284618025@qq.com <284618025@qq.com>
2025-07-17 13:55:03 +0800

    fix:改进页面展示的方式，方便一目了然之前搞过啥

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,18 @@
 <template>
 
     <div>
-        <button @click="shuffleList">shuffle</button>
-        <TransitionGroup class="wraps" tag="div">
-            <div class="item" :key="item.id" v-for="item in list">
-                {{ item.number }}
-            </div>
-        </TransitionGroup>
+        <button class="bem-button" type="button" @click="$router.push('/')">返回主页</button>
+        <!-- 路由内容将在这里渲染，被注释了的话就不会路由转发 -->
+        <router-view></router-view>
     </div>
-    <div class="bem-test">goooood</div>
 
 </template>
 
-<script setup lang='ts'>
-
-    import { ref } from 'vue';
-    import _ from 'lodash';
-
-    let list = ref(Array.apply(null, { length: 81 } as number[]).map((_, index) => {
-        return {
-            id: index,
-            // 对9取余，得到1-9的循环数字
-            number: (index % 9) + 1
-        }
-    }))
-
-    function shuffleList() {
-        list.value = _.shuffle(list.value)
-    }
-</script>
-
-<style lang='scss'>
-    .wraps {
-        display: flex;
-        flex-wrap: wrap;
-        width: calc(25px * 9);
-
-        .item {
-            width: 25px;
-            height: 25px;
-            border: 1px solid #ccc;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-    }
-
-    @include b(test) {
+<style lang="scss">
+    @include b(button) {
+        position: fixed;
+        left: 20px;
+        top: 20px;
         color: red;
     }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import 主页 from '../views/主页显示内容页面.vue'
+import 洗牌 from '../views/使用loadsh进行洗牌.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: '主页',
+      component: 主页,
+    },
+    {
+      path: '/shuffle',
+      name: '洗牌',
+      component: 洗牌,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
