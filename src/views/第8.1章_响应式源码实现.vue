d06c2ae5dad362a9e62827678096845b7a82f7ce
LeiBingbing <1481300095@qq.com>
2026-03-11 09:28:18 +0800

    refactor(响应式源码实现): 移除组件中的响应式测试代码
    将响应式实现测试代码注释掉，因为该实现不应在组件中进行

--- src/views/第8.1章_响应式源码实现.vue
+++ src/views/第8.1章_响应式源码实现.vue
@@ -1,33 +1,33 @@
 <template>
     <div>
-        <h2>响应式源码实现测试</h2>
-        <p>姓名：{{ Man1.name }}</p>
+        <h2>响应式源码实现测试，但不能在组件中实现</h2>
+        <!-- <p>姓名：{{ Man1.name }}</p>
         <p>年龄：{{ Man1.age }}</p>
         <button @click="changeName">修改姓名</button>
-        <button @click="changeAge">修改年龄</button>
+        <button @click="changeAge">修改年龄</button> -->
     </div>
 
 </template>
 
 <script setup lang='ts'>
-import { MyReactive, effect } from '@/utils/MyReactive'
+// import { MyReactive, effect } from '@/utils/MyReactive'
 
-const Man1 = MyReactive({ name: '张三', age: 18 })
+// const Man1 = MyReactive({ name: '张三', age: 18 })
 
-// 测试响应式
-effect(() => {
-  console.log(`姓名：${Man1.name} 年龄：${Man1.age}`)
-})
+// // 测试响应式
+// effect(() => {
+//   console.log(`姓名：${Man1.name} 年龄：${Man1.age}`)
+// })
 
-// 测试修改姓名
-const changeName = () => {
-  Man1.name = '李四'
-}
+// // 测试修改姓名
+// const changeName = () => {
+//   Man1.name = '李四'
+// }
 
-// 测试修改年龄
-const changeAge = () => {
-  Man1.age = 20
-}
+// // 测试修改年龄
+// const changeAge = () => {
+//   Man1.age = 20
+// }
 
 </script>
 
